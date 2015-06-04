e622325a142a16d97880543d896de171a692bc04
David Zhu <hdavidzhu@gmail.com>
2015-06-04 10:03:46 -0400

    With dummy POST request.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,12 +1,31 @@
 angular.module('starter.controllers', [])
   .controller('PearlCtrl', PearlCtrl);
 
-function PearlCtrl() {
+PearlCtrl.$inject = ["$http"];
+
+function PearlCtrl($http) {
   var vm = this;
 
+  vm.message = "";
   vm.sendInformation = sendInformation;
 
   function sendInformation(){
-    console.log("I am being clicked!");
+    console.log(vm.message);
+
+    // TODO: Implement POST request.
+    var url = "http://localhost:3000/test";
+    var msg = vm.message;
+
+    $http.post(url, {msg:msg}).
+      success(function(data, status, headers, config) {
+        // this callback will be called asynchronously
+        // when the response is available
+      }).
+      error(function(data, status, headers, config) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+      });
+
+    vm.message = "";
   }
 }
