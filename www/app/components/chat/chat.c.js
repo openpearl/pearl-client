571dc820644e19967c77115632dfdd180dee3a3c
David Zhu <hdavidzhu@gmail.com>
2015-06-09 17:51:40 -0400

    Can now get pedometer information.

--- www/app/components/chat/chat.c.js
+++ www/app/components/chat/chat.c.js
@@ -20,15 +20,16 @@ function ChatController(
     console.log("Platform is ready here.");
     $cordovaHealthKit.isAvailable().then(
       function(yes) {
-        var permissions = ['HKQuantityTypeIdentifierHeight'];
-        console.log(permissions);      
+        var readPermissions = ['HKQuantityTypeIdentifierStepCount'];
+        var writePermissions = [];
+        console.log(readPermissions);      
         
         $cordovaHealthKit.requestAuthorization(
-          permissions,
-          permissions
+          readPermissions,
+          readPermissions
         ).then(function(success){
           console.log("Requested permissions to read and write health information.");
-        });
+         });
       },
 
       function(no) {
@@ -38,11 +39,33 @@ function ChatController(
 
   var vm = this;
 
+  vm.getStepCount = getStepCount;
+
   vm.message = "";
   vm.sendInformation = sendInformation;
   vm.chatMessages = [];
 
+  function getStepCount(){
+    console.log("Getting step count.");
+
+    var m = moment().startOf('day');  
+    var endDate = m.toDate();  
+    var startDate = moment(m).subtract('d', 1).toDate();  
+
+    window.plugins.healthkit.sumQuantityType({
+      'startDate': startDate,
+      'endDate': endDate,
+      'sampleType': 'HKQuantityTypeIdentifierStepCount'
+    }, function(value) {
+      console.log("HealthKit Step Count Success: " + value);
+    }, function () {
+      console.log("HealthKit Step Count Query unsuccessful.");
+    });
+  }
+
   function sendInformation(){
+    vm.getStepCount();
+
     console.log(vm.message);
 
     // TODO: Add data to speech bubble.
@@ -51,18 +74,22 @@ function ChatController(
     $ionicScrollDelegate.scrollBottom(true);
 
     // TODO: Implement POST request.
-    var url = "http://localhost:3000/test";
+    var url = "https://odmmjjialz.localtunnel.me/api/v1/tests/";
     var msg = vm.message;
 
-    // $http.post(url, {msg:msg}).
-    //   success(function(data, status, headers, config) {
-    //     // this callback will be called asynchronously
-    //     // when the response is available
-    //   }).
-    //   error(function(data, status, headers, config) {
-    //     // called asynchronously if an error occurs
-    //     // or server returns response with an error status.
-    //   });
+    $http.post(url, {message: msg}).
+      success(function(data, status, headers, config) {
+        // this callback will be called asynchronously
+        // when the response is available
+
+        console.log(data.message);
+        vm.chatMessages.push(data.message);
+
+      }).
+      error(function(data, status, headers, config) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+      });
 
     vm.message = "";
   }
