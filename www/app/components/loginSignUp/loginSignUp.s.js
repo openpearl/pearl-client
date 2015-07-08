1a8859f02f38455536eae1c632fdca0718a61910
David Zhu <hdavidzhu@gmail.com>
2015-07-08 16:14:52 -0400

    Monkey patch for logging in. Change for future.

--- www/app/components/loginSignUp/loginSignUp.s.js
+++ www/app/components/loginSignUp/loginSignUp.s.js
@@ -10,7 +10,26 @@ module.exports = function(app) {
   ]);
 }
 
-function submitLogin() {
+function submitLogin($http) {
+
+  function postInfo(route, jsonObj, callback) {
+
+    console.log("Posting info.");
+
+    $http.post(route, jsonObj).
+      success(function(data, status, headers, config) {
+        // this callback will be called asynchronously
+        // when the response is available
+        console.log("Logging in successful.");
+        console.log(data.message);
+        callback();
+      }).
+      error(function(data, status, headers, config) {
+        console.error("Error sending login-signup.");
+        console.log(jsonObj);
+      });
+  }
+
   return function(email, password, callback) {
 
     console.log(callback);
@@ -31,7 +50,7 @@ function submitLogin() {
     }
 
     // Complete POST request and redirect.
-    var route = CURRENT_HOST + "/api/v1/login/";
+    var route = CURRENT_HOST + "/api/v1/auth/sign_in/";
     var loginJson = {
       email: email,
       password: password
@@ -41,7 +60,26 @@ function submitLogin() {
   } 
 }
 
-function submitSignup() {
+function submitSignup($http) {
+
+  function postInfo(route, jsonObj, callback) {
+
+    console.log("Posting info.");
+
+    $http.post(route, jsonObj).
+      success(function(data, status, headers, config) {
+        // this callback will be called asynchronously
+        // when the response is available
+        console.log("Logging in successful.");
+        console.log(data.message);
+        callback();
+      }).
+      error(function(data, status, headers, config) {
+        console.error("Error sending login-signup.");
+        console.log(jsonObj);
+      });
+  }
+
   return function(name, email, password, confirmPassword, callback) {
     // Validate all information.
 
@@ -66,31 +104,17 @@ function submitSignup() {
     }
 
     // Complete POST request and redirect.
-    var route = CURRENT_HOST + "/api/v1/login/";
+    var route = CURRENT_HOST + "/api/v1/auth/";
     var signUpJson = {
       name: name,
       email: email,
-      password: password
+      password: password,
+      password_confirmation: password,
+
+      // FIXME: Change this link to something reasonable.
+      confirm_success_url: "http://www.google.com/"
     }
 
     postInfo(route, signUpJson, callback);
   }
-}
-
-function postInfo(route, jsonObj, callback) {
-
-  console.log("Posting info.");
-
-  // $http.post(route, loginJson).
-  //   success(function(data, status, headers, config) {
-  //     // this callback will be called asynchronously
-  //     // when the response is available
-  //     console.log("Logging in successful.");
-  //     console.log(data.message);
-      callback();
-  //   }).
-  //   error(function(data, status, headers, config) {
-  //     alert("Error logging in.");
-  //     console.log("Error logging  in.");
-  //   });
 }
